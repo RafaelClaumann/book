4a94cf824f70f0efe0f91c9c42400d85aa1e8e77
Rafael Claumann Bernardes <rclauman@everis.com>
2020-12-23 17:16:37 -0300

    MultiplicationController e primeiro teste seguindo TDD

---
+++ social-multiplication/src/main/java/microservices/book/multiplication/controller/MultiplicationController.java
@@ -0,0 +1,18 @@
+package microservices.book.multiplication.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import microservices.book.multiplication.service.MultiplicationService;
+
+@RestController
+public class MultiplicationController {
+	
+	private final MultiplicationService multiplicationService;
+
+	@Autowired
+	public MultiplicationController(final MultiplicationService multiplicationService) {
+		this.multiplicationService = multiplicationService;
+	}
+	
+}
