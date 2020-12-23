ea63008035c5bb36a4af9d5d45d9f1caadf8af35
Rafael Claumann Bernardes <rclauman@everis.com>
2020-12-23 15:28:33 -0300

    refatorando testes, agora sem spring context

---
+++ social-multiplication/src/main/java/microservices/book/multiplication/service/impl/RandomGeneratorServiceImpl.java
@@ -0,0 +1,17 @@
+package microservices.book.multiplication.service.impl;
+
+import java.util.Random;
+
+import microservices.book.multiplication.service.RandomGeneratorService;
+
+public class RandomGeneratorServiceImpl implements RandomGeneratorService {
+
+	final static int MINIMUM_FACTOR = 11;
+	final static int MAXIMUM_FACTOR = 99;
+
+	@Override
+	public int generateRandomFactor() {
+		return new Random().nextInt((MAXIMUM_FACTOR - MINIMUM_FACTOR) + 1) + MINIMUM_FACTOR;
+	}
+
+}
