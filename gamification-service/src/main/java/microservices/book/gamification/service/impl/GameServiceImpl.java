9e76a3c85c16314a8495cefa2498414d29611a7c
Rafael Claumann Bernardes <rclauman@everis.com>
2021-01-07 10:40:55 -0300

    versao 2 processFirstCorrectAttemptTest() e newAttemptForUser( )

--- gamification-service/src/main/java/microservices/book/gamification/service/impl/GameServiceImpl.java
+++ gamification-service/src/main/java/microservices/book/gamification/service/impl/GameServiceImpl.java
@@ -2,21 +2,83 @@ package microservices.book.gamification.service.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import microservices.book.gamification.domain.BadgeCard;
 import microservices.book.gamification.domain.GameStats;
+import microservices.book.gamification.domain.ScoreCard;
 import microservices.book.gamification.domain.enums.Badge;
+import microservices.book.gamification.repository.BadgeCardRepository;
+import microservices.book.gamification.repository.ScoreCardRepository;
 import microservices.book.gamification.service.GameService;
 
 @Service
 public class GameServiceImpl implements GameService {
 
+	private ScoreCardRepository scoreCardRepository;
+
+	private BadgeCardRepository badgeCardRepository;
+
+	@Autowired
+	public GameServiceImpl(final ScoreCardRepository scoreCardRepository,
+			final BadgeCardRepository badgeCardRepository) {
+		this.scoreCardRepository = scoreCardRepository;
+		this.badgeCardRepository = badgeCardRepository;
+	}
+
 	@Override
 	public GameStats newAttemptForUser(Long userId, Long attemptId, boolean correct) {
-		List<Badge> badges = new ArrayList<>();
-		badges.add(Badge.FIRST_ATTEMPT);
-		return new GameStats(userId, 10, badges);
+
+		
+		if (correct) {
+			// vai criar um ScoreCard com 10pts; usando System.currentTimeMillis()
+			ScoreCard scoreCard = new ScoreCard(userId, attemptId);
+			scoreCardRepository.save(scoreCard);
+
+			// atualizar os badges
+			List<BadgeCard> badgeCards = processBadges(userId);
+			List<Badge> badges = badgeCards.stream().map(BadgeCard::getBadge).collect(Collectors.toList());
+			
+			return new GameStats(userId, scoreCard.getScore(), badges);
+		}
+
+		return GameStats.emptyStats(userId);
+	}
+
+	public List<BadgeCard> processBadges(Long userId) {
+
+		List<BadgeCard> badgeCards = new ArrayList<>();
+		int totalScoreForUser = scoreCardRepository.getTotalScoreForUser(userId);
+		List<ScoreCard> scoreCards = scoreCardRepository.findByUserIdOrderByScoreTimestampDesc(userId);
+		
+		if(scoreCards.size() == 1 && !containsBadge(badgeCards, Badge.FIRST_ATTEMPT)) {
+			badgeCards.add(new BadgeCard(userId, Badge.FIRST_ATTEMPT));
+			badgeCardRepository.save(new BadgeCard(userId, Badge.FIRST_ATTEMPT));
+		}
+
+		if(totalScoreForUser >= 100 && !containsBadge(badgeCards, Badge.BRONZE_MULTIPLICATOR)) {
+			badgeCards.add(new BadgeCard(userId, Badge.BRONZE_MULTIPLICATOR));
+			badgeCardRepository.save(new BadgeCard(userId, Badge.BRONZE_MULTIPLICATOR));
+		}
+		
+		if(totalScoreForUser >= 500 && !containsBadge(badgeCards, Badge.SILVER_MULTIPLICATOR)) {
+			badgeCards.add(new BadgeCard(userId, Badge.SILVER_MULTIPLICATOR));
+			badgeCardRepository.save(new BadgeCard(userId, Badge.SILVER_MULTIPLICATOR));
+		}
+		
+		if(totalScoreForUser >= 999 && !containsBadge(badgeCards, Badge.GOLD_MULTIPLICATOR)) {
+			badgeCards.add(new BadgeCard(userId, Badge.GOLD_MULTIPLICATOR));
+			badgeCardRepository.save(new BadgeCard(userId, Badge.GOLD_MULTIPLICATOR));
+		}
+
+		return badgeCards;
+	}
+
+	public boolean containsBadge(List<BadgeCard> badgeCards, Badge badge) {
+		return badgeCards.stream().anyMatch(card -> card.getBadge().equals(badge));
 	}
 
 	@Override
@@ -26,3 +88,4 @@ public class GameServiceImpl implements GameService {
 	}
 
 }
+;
--- gamification-service/src/test/java/microservices/book/gamification/service/impl/GameServiceImplTest.java
+++ gamification-service/src/test/java/microservices/book/gamification/service/impl/GameServiceImplTest.java
@@ -3,35 +3,59 @@ package microservices.book.gamification.service.impl;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.any;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import microservices.book.gamification.domain.GameStats;
+import microservices.book.gamification.domain.ScoreCard;
 import microservices.book.gamification.domain.enums.Badge;
+import microservices.book.gamification.repository.BadgeCardRepository;
+import microservices.book.gamification.repository.ScoreCardRepository;
 
 class GameServiceImplTest {
 
 	private GameServiceImpl gameService;
 
+	@Mock
+	private ScoreCardRepository scoreCardRepository;
+
+	@Mock
+	private BadgeCardRepository badgeCardRepository;
+
 	@BeforeEach
 	void setUp() {
-		gameService = new GameServiceImpl();
+		MockitoAnnotations.initMocks(this);
+		gameService = new GameServiceImpl(scoreCardRepository, badgeCardRepository);
 	}
 
 	@Test
 	void processFirstCorrectAttemptTest() {
 		Long userId = 1L;
 		Long attemptId = 1L;
-		GameStats firstGameStats = gameService.newAttemptForUser(userId, attemptId, true);
-		
-		assertThat(firstGameStats.getUserId()).isEqualTo(userId);
-		assertThat(firstGameStats.getScore()).isEqualTo(10);
-		assertThat(firstGameStats.getBadges()).contains(Badge.FIRST_ATTEMPT);
+		int userTotalScore = 10;
+		ScoreCard scoreCard = new ScoreCard(userId, attemptId);
+
+		given(scoreCardRepository.getTotalScoreForUser(userId)).willReturn(userTotalScore);
+
+		given(scoreCardRepository.findByUserIdOrderByScoreTimestampDesc(userId))
+				.willReturn(Collections.singletonList(scoreCard));
+		given(badgeCardRepository.findByUserIdOrderByBadgeTimestampDesc(userId))
+				.willReturn(Collections.emptyList());
+
+		GameStats interation = gameService.newAttemptForUser(userId, attemptId, true);
+
+		assertThat(interation.getScore()).isEqualTo(scoreCard.getScore());
+		assertThat(interation.getBadges()).containsOnly(Badge.FIRST_ATTEMPT);
 	}
 
 }
