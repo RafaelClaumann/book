e3b154277705b808de4aac635227d5e8fdffe91d
Rafael Claumann Bernardes <rclauman@everis.com>
2021-01-12 10:57:38 -0300

    controllers/endpoints do microservico gamification

---
+++ gamification-service/src/main/java/microservices/book/gamification/controller/UserStatsController.java
@@ -0,0 +1,30 @@
+package microservices.book.gamification.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import microservices.book.gamification.domain.GameStats;
+import microservices.book.gamification.service.GameService;
+
+@RestController
+@RequestMapping(value = "/stats")
+public class UserStatsController {
+
+	private GameService gameService;
+
+	@Autowired
+	public UserStatsController(final GameService gameService) {
+		this.gameService = gameService;
+	}
+
+	@GetMapping
+	public ResponseEntity<GameStats> getUserStats(@PathVariable Long userId) {
+		GameStats retrieveStatsForUser = gameService.retrieveStatsForUser(userId);
+		return ResponseEntity.ok(retrieveStatsForUser);
+	}
+
+}
